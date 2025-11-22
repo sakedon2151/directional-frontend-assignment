c070a08cd49a248b53f7acc20a6ee457f7a86db7
Sangmin Park <sakedon2151@gmail.com>
2025-11-23 07:45:37 +0900

    Change background color of LoginScreen

--- screens/login/LoginScreen.tsx
+++ screens/login/LoginScreen.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from '@/components/login/LoginForm';
 
 export const LoginScreen = () => {
   return (
-    <div className="flex h-dvh w-full justify-end bg-red-400">
+    <div className="flex h-dvh w-full justify-end bg-slate-800">
       <LoginForm className="m-4 w-full justify-center md:ml-0 md:w-1/2" />
     </div>
   );
