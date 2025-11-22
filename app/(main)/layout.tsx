7c3a436e757370d2e81a0e100b39ad4ed49d6541
Sangmin Park <sakedon2151@gmail.com>
2025-11-23 02:27:56 +0900

    feat/하위 페이지 컴포넌트, 레이아웃 구성/#4

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,14 @@
+import { AppSidebar } from '@/components/common/AppSidebar';
+import { SidebarProvider, SidebarTrigger } from '@/components/common/Sidebar';
+
 export default function MainLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <SidebarProvider>
+      <AppSidebar></AppSidebar>
+      <main className="w-full">
+        <SidebarTrigger className="m-2 size-8" />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
 }
